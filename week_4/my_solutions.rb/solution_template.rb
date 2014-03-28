030bf76a1ad27e67ab7856325b5496c7b8cc01ac
Maria Piper <maria@devbootcamp.com>
2014-03-28 13:52:37 -0700

    added a solution template

---
+++ week_4/my_solutions.rb/solution_template.rb
@@ -0,0 +1,39 @@
+# U2.W#: Title here
+
+
+# I worked on this challenge [by myself, with: ].
+
+# 2. Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# 3. Initial Solution
+
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection 
+
+
+
+
+
