af2552a431ac28f82504f1d6e97f75dff892a195
Maria Piper <maria@devbootcamp.com>
2014-04-05 19:34:27 -0700

    added boggle board code to challenge template:

---
+++ week_5/4_boggle_board/my_solution.rb
@@ -0,0 +1,59 @@
+# U2.W5: A Nested Array to Model a Boggle Board
+
+boggle_board = [["b", "r", "a", "e"],
+                ["i", "o", "d", "t"],
+                ["e", "c", "l", "r"],
+                ["t", "a", "k", "e"]]
+
+# I worked on this challenge [by myself, with: ].
+
+# Part 1: Access multiple elements of a nested array
+
+# Pseudocode
+
+# Initial Solution
+
+
+# Refactored Solution
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+
+
+# Reflection 
+
+#-------------------------------------------------------------------------------
+
+# Part 2: Write a method that takes a row number and returns all the elements in the row.  
+
+# Pseudocode
+
+# Initial Solution
+
+
+# Refactored Solution
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+
+
+# Reflection 
+
+
+
+#-------------------------------------------------------------------------------
+
+# Part 3: Now write a method that takes a column number and returns all the elements in the column.
+
+# Pseudocode
+
+# Initial Solution
+
+
+# Refactored Solution
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+
+
+# Reflection 
