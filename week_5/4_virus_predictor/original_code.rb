4bb328767e38dfe74c7147858e598cb0d7647a83
Maria Piper <maria@devbootcamp.com>
2014-04-05 19:10:01 -0700

    added content

---
+++ week_5/4_virus_predictor/original_code.rb
@@ -0,0 +1,81 @@
+# U2.W5: Virus Predictor
+
+# I worked on this challenge [by myself, with: ].
+
+# EXPLANATION OF require_relative
+#
+#
+require_relative 'state_data'
+
+class VirusPredictor
+
+  def initialize(state_of_origin, population_density, population, region, regional_spread)
+    @state = state_of_origin
+    @population = population
+    @population_density = population_density
+    @region = region
+    @next_region = regional_spread
+  end
+
+  def virus_effects  #HINT: What is the SCOPE of instance variables?
+    predicted_deaths(@population_density, @population, @state)
+    speed_of_spread(@population_density, @state)
+  end
+
+  private  #what is this?  what happens if it were cut and pasted above the virus_effects method
+
+  def predicted_deaths(population_density, population, state)
+    if @population_density >= 200
+      number_of_deaths = (@population * 0.4).floor
+    elsif @population_density >= 150
+      number_of_deaths = (@population * 0.3).floor
+    elsif @population_density >= 100
+      number_of_deaths = (@population * 0.2).floor
+    elsif @population_density >= 50
+      number_of_deaths = (@population * 0.1).floor
+    else 
+      number_of_deaths = (@population * 0.05).floor
+    end
+
+    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+
+  end
+
+  def speed_of_spread(population_density, state) #in months
+    speed = 0.0
+
+    if @population_density >= 200
+      speed += 0.5
+    elsif @population_density >= 150
+      speed += 1
+    elsif @population_density >= 100
+      speed += 1.5
+    elsif @population_density >= 50
+      speed += 2
+    else 
+      speed += 2.5
+    end
+
+    puts " and will spread across the state in #{speed} months.\n\n"
+
+  end
+
+end
+
+#=======================================================================
+
+# DRIVER CODE
+ # initialize VirusPredictor for each state
+
+
+alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population], STATE_DATA["Alabama"][:region], STATE_DATA["Alabama"][:regional_spread]) 
+alabama.virus_effects
+
+jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population], STATE_DATA["New Jersey"][:region], STATE_DATA["New Jersey"][:regional_spread]) 
+jersey.virus_effects
+
+california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population], STATE_DATA["California"][:region], STATE_DATA["California"][:regional_spread]) 
+california.virus_effects
+
+alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population], STATE_DATA["Alaska"][:region], STATE_DATA["Alaska"][:regional_spread]) 
+alaska.virus_effects
--- week_5/4_virus_predictor/readme.md
+++ week_5/4_virus_predictor/readme.md
@@ -1,6 +1,6 @@
 [Week 5 Home](./)
 
-# U2.W5: 
+# U2.W5: Virus Predictor
 
 
 ## Learning Objectives
@@ -10,13 +10,18 @@
 
 ## Background
 
+The C.D.C. (Centers for Disease Control) is concerned about a recent form of deadly influenza. They've implemented the following program to predict the effect of the virus by state. Eventually they want to predict the spread of the virus, which is why that data is included.
+
+The initial program is not very D.R.Y., but the program designer went on vacation and your boss needs the model to work efficiently so researchers can use it. You've been brought in to make the code more efficient and expandable for later features such as incorporating demographic data. 
+
 
 ## Directions
  
-1. Write [Driver Test Code](../references/driver_code.md)
-2. [Pseudocode](../references/pseudocode.md)
-3. [Initial Solution](../references/initial_solution.md)
-4. [Refactored Solution](../references/refactoring.md)
-5. [Reflect](../references/reflection_guidelines.md)
-6. Sync your changes (push your solution) to Github
-7. [Review](../references/review.md)
+1. Run the code.  Look at the output.  Look at the input (it's in the other file).  Explain what the program is doing.
+2. Write a comment explaining the require_relative statement - You should be somewhat familiar with this by now.
+3. Comment each method and define it's responsibility.
+4. New Feature: create a report for all 50 states, not just the 4 listed below.  Is there a DRY way of doing this?
+5. Refactor the virus_effects method.  (HINT: what is the scope of instance variables?)
+6. What is the purpose of "private". What happens if you move it elsewhere in the class?
+7. Refactor the private methods predicted_deaths and speed_of_spread.  How can you make them more DRY?
+8. BONUS: Access the population by calling it on the instance.
