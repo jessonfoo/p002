01eb089e1ef7968eece2267e76f18816245a58c9
Maria Piper <maria@devbootcamp.com>
2014-04-05 18:00:33 -0700

    added initial code

---
+++ week_5/2_die/my_solution.rb
@@ -0,0 +1,42 @@
+# U2.W5: Die Class 2: Arbitrary Symbols
+
+
+# I worked on this challenge [by myself, with: ].
+
+# 2. Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# 3. Initial Solution
+
+class Die
+  def initialize(labels)
+  end
+
+  def sides
+  end
+
+  def roll
+  end
+end
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection 
