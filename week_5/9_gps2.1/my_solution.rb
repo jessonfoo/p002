1aaac7f89a2dddb6a79a27be85aec43e28c6140c
Maria Piper <maria@devbootcamp.com>
2014-04-05 21:52:07 -0700

    added place for bakery challenge solution

---
+++ week_5/9_gps2.1/my_solution.rb
@@ -0,0 +1,39 @@
+# U2.W5: Bakery Challenge GPS
+
+# I worked on this challenge with: 
+
+
+
+
+
+
+# Our Refactored Solution
+
+
+
+
+
+
+
+
+
+#DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
+# These are the tests to ensure it's working. 
+# These should all print true if the method is working properly.
+p bakery_num(24, "cake") == "You need to make 4 cake(s)."
+p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)."
+p bakery_num(24, "cookie") == "You need to make 24 cookie(s)."
+p bakery_num(4, "pie") == "You need to make 0 pie(s), 0 cake(s), and 4 cookie(s)."
+p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie(s)."
+# p bakery_num(3, "apples") # this will raise an ArgumentError
+
+# You SHOULD change this driver code. Why? Because it doesn't make sense.
+p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
+
+
+
+
+#  Reflection 
+
+
+
