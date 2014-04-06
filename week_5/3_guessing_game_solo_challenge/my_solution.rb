2d217d5d8050626bbc2fc02d092e7d79cf9462fa
Maria Piper <maria@devbootcamp.com>
2014-04-05 17:55:30 -0700

    added initial code

---
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -0,0 +1,40 @@
+# U2.W5: Build a simple guessing game SOLO CHALLENGE
+
+
+# I worked on this challenge [by myself, with: ].
+
+# 2. Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# 3. Initial Solution
+
+class GuessingGame
+  def initialize(answer)
+    # Your initialization code goes here
+  end
+  
+  # Make sure you define the other required methods, too
+end
+
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection 
