875f3a69a6a86373f3f65b8e095f9e6edee4d278
Maria Piper <maria@devbootcamp.com>
2014-04-10 16:12:48 -0700

    added challenge instructions and initial code

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -1,4 +1,4 @@
-# U2.W6: 
+# U2.W6: Create a BoggleBoard Class
 
 
 # I worked on this challenge [by myself, with: ].
@@ -9,8 +9,20 @@
 
 
 # 3. Initial Solution
-
-
+class BoggleBoard
+ 
+  #your code here
+ 
+end
+ 
+ 
+dice_grid = [["b", "r", "a", "e"],
+             ["i", "o", "d", "t"],
+             ["e", "c", "l", "r"],
+             ["t", "a", "k", "e"]]
+ 
+boggle_board = BoggleBoard.new(dice_grid)
+ 
 
 
 # 4. Refactored Solution
@@ -21,10 +33,8 @@
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
-
+# create driver test code to retrieve a value at a coordinate here:
+# implement tests for each of the methods here:
 
 
 # 5. Reflection 
