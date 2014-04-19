9c54a8ae48d6e82d9e158e254bfcfb6041c22959
Maria Piper <maria@devbootcamp.com>
2014-04-19 15:23:23 -0700

    added template

---
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -0,0 +1,36 @@
+# U2.W4: Homework Cheater!
+
+
+# I worked on this challenge [by myself, with: ].
+
+# 2. Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# 3. Initial Solution
+
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection 
+
+
