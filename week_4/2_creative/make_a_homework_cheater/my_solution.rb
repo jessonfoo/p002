157bc4fe016a4803727fff82c5e624e248eb8433
jessonfoo <jessonfoo@gmail.com>
2014-06-03 16:59:49 -0700

    added my solution

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -5,34 +5,37 @@
 
 # 2. Pseudocode
 
-# Input:
+# Input: 
+# my code will take a title
 # Output:
 # Steps:
 
 
 # 3. Initial Solution
+
 def essay_writer(title, topic, date, thesis_statment, pronoun)
-	# @pronoun = pronoun
 	 if pronoun === "male"
 		return "#{topic} was an important person in #{date}. He did a lot. I want to learn more about him. #{thesis_statment} #{topic}'s contribution was important."
-	 else
+	 elsif pronun === "female"
 	 	return "#{topic} was an important person in #{date}. She did a lot. I want to learn more about her. #{thesis_statment} #{topic}'s contribution was important."
+	 elsif pronun === "place"
+	 	return "#{topic} was an important place in #{date}.  It was very influential. I want to learn more about it. #{thesis_statment} #{topic}'s contribution was important."
+	 elsif pronoun === "generic"
+	 	return "#{topic} was an important topic in #{date}.  It was very influential. I want to learn more about it. #{thesis_statment} #{topic}'s contribution was important."
 	 end
+end
+essay_writer("The First Shogun", "Tokugawa Ieyasu", 1603, "His most important contribution to history is that he founded the Tokugawa period, a peaceful time that lasted over 200 years.", "male") 
+
+# 4. Refactored Solution
+
+def essay_writer(title, topic, date, thesis_statment, pronoun)
+	male = ["person", "he", "him"]
+	female = ["person", "she", "her"]
+	place =["place", "it", "it"]
+	if pronoun === male
 end
 
 essay_writer("The First Shogun", "Tokugawa Ieyasu", 1603, "His most important contribution to history is that he founded the Tokugawa period, a peaceful time that lasted over 200 years.", "male") 
-
-#=> My Award-Winning Essay (or maybe "D"-worthy)!
-# The First Shogun
-
-# Ieyasu Tokugawa was an important person in 1603. He did a lot. I want to learn more about him. 
-# His most important contribution to history is that he founded the Tokugawa period, a peaceful 
-# time that lasted over 200 years. Tokugawa's contribution is important.
-
-
-
-
-# 4. Refactored Solution
 
 
 
