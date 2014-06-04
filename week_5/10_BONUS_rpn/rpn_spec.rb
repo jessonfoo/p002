92fc6076998f59927115f61e4a4e9471a90df9ea
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-06-04 11:10:09 -0700

    fixed RPN calc spec

--- week_5/10_BONUS_rpn/rpn_spec.rb
+++ week_5/10_BONUS_rpn/rpn_spec.rb
@@ -5,49 +5,49 @@ describe RPNCalculator do
 
   describe '#evaluate' do
     it "expects 1 argument" do
-      RPNCalculator.instance_method(:evaluate).arity.should eq 1
+      expect(RPNCalculator.instance_method(:evaluate).arity).to eq 1
     end
     
     it "correctly evaluates '0'" do
-      rpn.evaluate('0').should be_zero
+      expect(rpn.evaluate('0')).to be_zero
     end
 
     it "correctly evaluates '-1'" do
-      rpn.evaluate('-1').should eq -1
+      expect(rpn.evaluate('-1')).to eq -1
     end
 
     it "supports addition" do
-      rpn.evaluate('1 1 +').should eq (1 + 1)
+      expect(rpn.evaluate('1 1 +')).to eq (1 + 1)
     end
     
     it "adds negative numbers" do
-      rpn.evaluate('1 -1 +').should be_zero
+      expect(rpn.evaluate('1 -1 +')).to be_zero
     end
 
     it "supports multiplication" do
-      rpn.evaluate('2 2 *').should eq (2 * 2)
+      expect(rpn.evaluate('2 2 *')).to eq (2 * 2)
     end
     
     it "subtracts in the correct order" do
-      rpn.evaluate('5 10 -').should eq (5 - 10)
+      expect(rpn.evaluate('5 10 -')).to eq (5 - 10)
     end
 
     it "handles multiple operators in a row" do
-      rpn.evaluate('1 2 3 4 + + +').should eq (1 + 2 + 3 + 4)
+      expect(rpn.evaluate('1 2 3 4 + + +')).to eq (1 + 2 + 3 + 4)
     end
 
     it "correctly combines addition and multiplication" do
-      rpn.evaluate('1 2 + 3 4 + *').should eq ((2 + 1) * (4 + 3))
+      expect(rpn.evaluate('1 2 + 3 4 + *')).to eq ((2 + 1) * (4 + 3))
     end
 
     it "correctly combines multiplication, addition, and subtraction" do
-      rpn.evaluate('20 10 5 4 + * -').should eq (20 - 10*(5 + 4))
+      expect(rpn.evaluate('20 10 5 4 + * -')).to eq (20 - 10*(5 + 4))
     end
 
     it "handles arbitrary input" do
       a, b, c = Array.new(3) { rand(100) }
       
-      rpn.evaluate("#{a} #{b} - #{c} *").should eq (a-b)*c
+      expect(rpn.evaluate("#{a} #{b} - #{c} *")).to eq (a-b)*c
     end
   end
 end
